4ac37e6b7ffb51aafc338ca7f47b04c4ef2e40b6
ajay-k <ajay_k@sbcglobal.net>
2015-12-18 12:34:32 -0800

    TMDB JSON working

--- moviePicker/ViewController.swift
+++ moviePicker/ViewController.swift
@@ -18,16 +18,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let siteAddress = "https://api.themoviedb.org/3/movie/550?api_key=c79b9571d2ab98df56637922cb4e93d5"
-        let url = NSURL (string: siteAddress)
-        let urlRequest = NSURLRequest(URL: url!)
+        Alamofire.request(.GET, "https://api.themoviedb.org/3/movie/550?api_key=c79b9571d2ab98df56637922cb4e93d5", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
         
-        let request = Alamofire.request(.GET, urlRequest)
         
-        print(request)
     }
-
-    override func didReceiveMemoryWarning() {
+}
+        override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
